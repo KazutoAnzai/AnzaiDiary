33d8b9825c82433c2ce491326fc34870c3f3076f
安西一途 <KazutoAnzai@AnzaiKazuto-no-MacBook-Pro.local>
2017-08-31 01:45:51 +0900

    first commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,17 @@
 class ArticlesController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :edit, :delete]
+
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+	def top
+		render :layout => 'top'
+	end
+
 	def index
-		@articles = Article.all
+		@articles = Article.all.order(id: "DESC")
 	end
 
 	def show
-		@article = Article.find(params[:id])
 	end
 
 	def new
@@ -13,12 +20,34 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
-		@article.save
+		if @article.save
+			redirect_to articles_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		@article.update(article_params)
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article.destroy
 		redirect_to articles_path
 	end
 
-private
+
+
+	private
 	def article_params
-		params.require(:article).permit(:title, :body, :author)
+		params.require(:article).permit(:title, :body, :picture, :author)
+	end
+
+	def set_article
+		@article = Article.find(params[:id])
 	end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+	validates :title, presence: true
+	validates :body, presence: true, length: { minimum: 5, message: 'Too short!!' }
+	validates :author, presence: true
+
+	has_many :comments, dependent: :destroy
+
+	mount_uploader :picture, PictureUploader
 end
